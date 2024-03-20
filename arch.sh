2554c963c7d175712e8df2920712bedbb4823307
protoblaster <protoblaster11@protonmail.com>
2024-03-20 14:50:17 +0000

    Update arch_psi.sh

--- arch.sh
+++ arch.sh
@@ -2,8 +2,55 @@
 
 PERMISSION=$EUID
 
-if [[ $PERMISSION == 0 ]]; then
-    echo -e "Congratulation... You are root!.."
-else
-    echo -e "YOU ARE NOT ROOT!.."
-fi
+check_permission() {
+  if [[ $PERMISSION == 0 ]]; then
+    echo -e "Congrats... You have root permissions... Proceeding...\n"
+    perform_update
+    install_packages
+  else
+    echo -e "Oops... You do not have root permissions! Exiting...\n"
+  fi
+}
+
+perform_update() {
+  read -p "Would you like to update the system (y/n): " choice
+
+  if [[ $choice == 'y' ]]; then
+    pacman -Syyu
+    if [[ $? == 0 ]]; then
+      echo "The system update was successful..."
+    else
+      echo "The update was not successful..."
+      read -p ""
+      exit
+    fi
+  else
+    echo "Not proceeding with the system update..."
+  fi
+}
+
+install_packages() {
+  packages=(
+    neovim
+    jdk-openjdk
+    intellij-idea-community-edition
+    kotlin
+    linux-headers
+    base-devel
+    chromium
+    firefox
+    hugo
+  )
+
+  for pkg in ${packages[@]}; do
+    sudo pacman -S $pkg
+  done
+
+  if [[ $? == 0 ]]; then
+    echo "The command completed successfully..."
+  else
+    echo "The command did not complete successfully..."
+  fi
+}
+
+check_permission
